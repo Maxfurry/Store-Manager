a8b15e963ae0f522489aaa5b2efdcb2f66492eb0
Maxfurry <32283153+Maxfurry@users.noreply.github.com>
2018-10-12 20:11:26 +0100

    Add elements to admin page

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -1,7 +1,44 @@
-function openMenu() {
-    document.getElementById("sideMenu").style.width = "200px";
+const users = document.getElementById("users");
+const user = document.getElementById("user");
+
+const items = document.getElementById("items");
+const item = document.getElementById("item");
+
+const records = document.getElementById("records");
+const record = document.getElementById("record");
+
+const categories = document.getElementById("categories");
+const category = document.getElementById("category");
+
+let showUser = () => {
+    users.style.display = "block";
+    items.style.display = "none";
+    records.style.display = "none";
+    categories.style.display = "none"
 }
 
-function closeMenu() {
-    document.getElementById("sideMenu").style.width = "0";
+let showItem = () => {
+    items.style.display = "block";
+    records.style.display = "none";
+    users.style.display = "none";
+    categories.style.display = "none"
 }
+
+let showRecord = () => {
+    records.style.display = "block";
+    users.style.display = "none";
+    items.style.display = "none";
+    categories.style.display = "none"  
+}
+
+let showCategory = () => {
+    records.style.display = "none";
+    users.style.display = "none";
+    items.style.display = "none";
+    categories.style.display = "block";  
+}
+
+user.addEventListener('click', showUser);
+item.addEventListener('click', showItem);
+record.addEventListener('click', showRecord);
+category.addEventListener('click', showCategory);
