a55507085020655bc97f0ba9700dbd7ed94b8dfd
Maxfurry <32283153+Maxfurry@users.noreply.github.com>
2018-10-31 10:45:10 +0100

    feat(functionality): modify and delete produt - create function to modify product - create function to dekete specific product [Finishes #161531302]

--- src/controller/products.js
+++ src/controller/products.js
@@ -1,114 +1,164 @@
-import func from '../middlewares/functions';
+import db from '../model/dbconfig';
 
 class Product {
   // Module that gets all products
   static fetchProducts(req, res) {
-    const products = func.readFile('products');
-
-    if (!products) {
-      return res.status(403).json({
-        success: false,
-        message: 'Request to get all product was not succesfull',
+    db.query('SELECT * from products', (err, data) => {
+      if (err) {
+        res.status(403).json({
+          success: false,
+          err,
+          message: 'Request to get all product was not succesfull',
+        });
+        return;
+      }
+      if (data.rowCount < 1) {
+        res.status(200).json({
+          success: false,
+          message: 'No product exist in database',
+        });
+        return;
+      }
+      res.status(200).json({
+        success: true,
+        data: data.rows,
+        message: 'Request to get all product successfull',
       });
-    }
-    return res.status(200).json({
-      success: true,
-      products,
-      message: 'Request to get all product successfull',
     });
   }
 
   // Module that gets specific product
   static fetchProduct(req, res) {
-    const product = func.readFile('products', req.params.productId);
-
-    if (!product) {
-      return res.status(403).json({
+    const id = req.params.productId;
+    db.query('SELECT * from products WHERE id = $1', [id], (err, data) => {
+      if (err) {
+        res.status(403).json({
+          success: false,
+          err,
+          message: 'Request to get specific product was not succesfull',
+        });
+        return;
+      }
+      if (data.rowCount > 0) {
+        res.status(200).json({
+          success: true,
+          info: data.rows,
+          message: 'Request to get all product successfull',
+        });
+        return;
+      }
+      res.status(403).json({
         success: false,
         message: 'Request to get specific product was not succesfull',
       });
-    }
-    return res.status(200).json({
-      success: true,
-      product,
-      message: 'Request to get all product successfull',
+    });
+  }
+
+  static productExist(req, res, next) {
+    db.query('SELECT * from products WHERE name = $1', [req.body.name], (err, data) => {
+      if (err) {
+        return err;
+      }
+      if (data.rowCount > 0) {
+        return res.status(403).send({
+          success: false,
+          message: 'Product already exist',
+        });
+      }
+      return next();
     });
   }
 
   // Module that create new product
   static createProduct(req, res) {
-    const product = {
-      name: req.body.name,
-      productId: req.body.productId,
-      category: req.body.category,
-      price: req.body.price,
-      quantity: req.body.quantity,
-    };
+    const product = [
+      req.body.name,
+      req.body.category,
+      req.body.price,
+      req.body.quantity,
+    ];
 
-    const updatedFile = func.updateFile('products', product, req.body.productId, 'create');
-
-    if (updatedFile === 'error') {
-      return res.status(403).json({
-        success: false,
-        message: 'Your request was not succesfull',
+    db.query('INSERT INTO products(name, category, price, quantity) VALUES($1,$2,$3,$4)', product, (err) => {
+      if (err) {
+        res.status(403).json({
+          success: false,
+          err,
+          message: 'Your request was not succesfull',
+        });
+        return;
+      }
+      res.status(200).json({
+        success: true,
+        product: {
+          name: req.body.name,
+          category: req.body.category,
+          price: req.body.price,
+          quantity: req.body.quantity,
+        },
+        message: 'Product Created Successfully',
       });
-    }
-
-    if (updatedFile === 'exist') {
-      return res.status(403).json({
-        success: false,
-        message: 'Product already exist',
-      });
-    }
-
-    return res.status(200).json({
-      success: true,
-      product,
-      message: 'Product Created Successfully',
     });
   }
 
   //  updates Products
   static updateProduct(req, res) {
-    const product = {
-      name: req.body.name,
-      productId: req.body.productId,
-      category: req.body.category,
-      price: req.body.price,
-      quantity: req.body.quantity,
-    };
+    const product = [
+      req.body.name,
+      req.body.category,
+      req.body.price,
+      req.body.quantity,
+      req.params.productId,
+    ];
 
-    const updatedFile = func.updateFile('products', product, req.body.productId, 'update');
-
-    if (updatedFile === 'error') {
-      return res.status(403).json({
-        success: false,
-        message: 'Your request was not succesfull',
+    db.query('UPDATE products SET name=$1, category=$2, price=$3, quantity=$4 WHERE id=$5', product, (err, data) => {
+      if (err) {
+        res.status(403).json({
+          success: false,
+          message: 'Your request was not succesfull, check product Id',
+        });
+        return;
+      }
+      if (data.rowCount < 1) {
+        res.status(200).json({
+          success: false,
+          message: 'Product Id does not exist',
+        });
+        return;
+      }
+      res.status(200).json({
+        success: true,
+        product: {
+          name: req.body.name,
+          category: req.body.category,
+          price: req.body.price,
+          quantity: req.body.quantity,
+        },
+        message: 'Product updated Successfully',
       });
-    }
-
-    return res.status(200).json({
-      success: true,
-      product,
-      message: 'Product updated Successfully',
     });
   }
 
   //  Module that delete user
   static deleteProduct(req, res) {
-    const deletedFile = func.deleteFile('products', req.body.productId);
-
-    if (deletedFile === 'error') {
-      return res.status(403).json({
-        success: false,
-        message: 'Your request was not succesfull',
+    db.query('DELETE from products WHERE id=$1', [req.params.productId], (err, data) => {
+      if (err) {
+        res.status(403).json({
+          success: false,
+          message: 'Your request was not succesfull',
+        });
+        return;
+      }
+      if (data.rowCount < 1) {
+        res.status(200).json({
+          success: false,
+          message: 'Product Id does not exist',
+        });
+        return;
+      }
+      res.status(200).json({
+        success: true,
+        message: 'Product Deleted Successfully',
       });
-    }
-
-    return res.status(200).json({
-      success: true,
-      product: deletedFile,
-      message: 'Product Deleted Successfully',
     });
   }
 }
--- src/route/products.js
+++ src/route/products.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import product from '../controller/products';
-//  import checkAuth from '../middlewares/checkAuth';
+import checkAuth from '../middlewares/checkAuth';
 import validate from '../middlewares/validate';
 
 const router = express.Router();
 
 router
-  .get('/', product.fetchProducts)
-  .get('/:productId', product.fetchProduct)
-  .post('/', validate.validateGeneral, validate.validateProduct, product.createProduct)
-  .patch('/', validate.validateGeneral, validate.validateProduct, product.updateProduct)
-  .delete('/', validate.validateGeneral, validate.validateProduct, product.deleteProduct);
+  .get('/', checkAuth.checkToken, product.fetchProducts)
+  .get('/:productId', checkAuth.checkToken, product.fetchProduct)
+  .post('/', checkAuth.checkAdmin, validate.validateGeneral, validate.validateProduct, product.productExist, product.createProduct)
+  .put('/:productId', checkAuth.checkAdmin, validate.validateGeneral, validate.validateProduct, product.updateProduct)
+  .delete('/:productId', checkAuth.checkAdmin, product.deleteProduct);
 
 export default router;
